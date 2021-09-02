fc4982fabf7f067fef191362f0301f701787b84f
Dan Reynolds <me@danreynolds.ca>
2021-09-02 13:03:03 -0400

    switch to useOnce over useMemo

--- src/hooks/useFragment.ts
+++ src/hooks/useFragment.ts
@@ -1,5 +1,6 @@
 import { getApolloContext, useQuery } from '@apollo/client';
-import { useContext, useMemo } from 'react';
+import { useContext } from 'react';
+import { useOnce } from './utils';
 import InvalidationPolicyCache from '../cache/InvalidationPolicyCache';
 import { DocumentNode } from 'graphql';
 import { buildWatchFragmentQuery } from '../client/utils';
@@ -13,11 +14,11 @@ export default function useFragment(fragment: DocumentNode, options: UseFragment
   const client = context.client;
   const cache = client?.cache as unknown as InvalidationPolicyCache;
 
-  const query = useMemo(() => buildWatchFragmentQuery({
+  const query = useOnce(() => buildWatchFragmentQuery({
     fragment,
     id: options.id,
     policies: cache.policies,
-  }), []);
+  }));
 
   return useQuery(query, {
     fetchPolicy: 'cache-only',
---
+++ src/hooks/utils.ts
@@ -0,0 +1,16 @@
+import { useRef } from 'react';
+import { isFunction } from 'lodash';
+
+export function useOnce<T>(value: T | (() => T)): T {
+  const valueRef = useRef<T>();
+  const hasCachedValueRef = useRef(false);
+  if (!hasCachedValueRef.current) {
+    if (isFunction(value)) {
+      valueRef.current = value();
+    } else {
+      valueRef.current = value;
+    }
+    hasCachedValueRef.current = true;
+  }
+  return valueRef.current as T;
+}
