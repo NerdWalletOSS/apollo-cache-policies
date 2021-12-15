93b25ef18c7382346ac0b4e9ed83383fcfda8e71
Dan Reynolds <me@danreynolds.ca>
2021-12-15 16:53:08 -0500

    add tests

--- tests/ApolloExtensionsClient.test.ts
+++ tests/ApolloExtensionsClient.test.ts
@@ -142,6 +142,94 @@ describe('ApolloExtensionsClient', () => {
         });
       });
     });
+
+    describe('with multiple fragments', () => {
+      describe('without a fragment name provided', () => {
+        test('should query for the first fragment', (done) => {
+          const observable = client.watchFragment({
+            fragment: gql`
+              fragment EmployeeFragment on Employee {
+                id
+                ...OtherFragment
+              }
+
+              fragment OtherFragment on Employee {
+                employee_name
+              }
+            `,
+            id: employee.toRef(),
+          });
+
+          const subscription = observable.subscribe((val) => {
+            expect(val).toEqual({
+              __typename: 'Employee',
+              id: employee.id,
+              employee_name: employee.employee_name,
+            });
+            // @ts-ignore Type policies is private
+            expect(Object.keys(cache.policies.typePolicies.Query.fields).length).toEqual(1);
+            subscription.unsubscribe();
+            // @ts-ignore Type policies is private
+            expect(Object.keys(cache.policies.typePolicies.Query.fields).length).toEqual(0);
+            done();
+          });
+        });
+      });
+
+      describe('with a fragmentName provided', () => {
+        describe('with a matching fragment name', () => {
+          test('should query for the fragment with the given fragment name', (done) => {
+            const observable = client.watchFragment({
+              fragment: gql`
+                fragment OtherFragment on Employee {
+                  employee_name
+                }
+
+                fragment EmployeeFragment on Employee {
+                  id
+                  ...OtherFragment
+                }
+              `,
+              id: employee.toRef(),
+              fragmentName: 'EmployeeFragment',
+            });
+
+            const subscription = observable.subscribe((val) => {
+              expect(val).toEqual({
+                __typename: 'Employee',
+                id: employee.id,
+                employee_name: employee.employee_name,
+              });
+              // @ts-ignore Type policies is private
+              expect(Object.keys(cache.policies.typePolicies.Query.fields).length).toEqual(1);
+              subscription.unsubscribe();
+              // @ts-ignore Type policies is private
+              expect(Object.keys(cache.policies.typePolicies.Query.fields).length).toEqual(0);
+              done();
+            });
+          });
+        });
+
+        describe('without a matching fragment name', () => {
+          test('should error that the given fragment could not be found', () => {
+            expect(() => client.watchFragment({
+              fragment: gql`
+                fragment OtherFragment on Employee {
+                  employee_name
+                }
+
+                fragment EmployeeFragment on Employee {
+                  id
+                  ...OtherFragment
+                }
+              `,
+              id: employee.toRef(),
+              fragmentName: 'MissingFragment',
+            })).toThrowError('No fragment with name: MissingFragment');
+          });
+        });
+      });
+    });
   });
 
   describe('watchFragmentWhere', () => {
